e9aa8c00e02637ba610fe36c89b9627f68429ac3
Sebastian Ariza <sebastian1ft200@gmail.com>
2025-10-04 18:47:57 +0000

    listo ya estan los dos archivos en la carpeta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark font-body antialiased">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
